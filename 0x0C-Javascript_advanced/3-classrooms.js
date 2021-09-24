114298843cdee083d2d404a8fa85310c143a7bcd
chedly99 <mohamed.chedliiy@gmail.com>
2021-09-24 18:40:56 +0100

    task3

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,17 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            seat()
+        }
+    }
+    let students = [];
+    for (let i = 0; i < numbersOfStudents; i++ ) {
+    students.push(studentSeat(i + 1))
+    }
+    return students;
+}
+const classRoom = createClassRoom(10);
+
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
