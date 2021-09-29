71f603255585fa3279c9589715b0e3656e7f5c13
chedly99 <mohamed.chedliiy@gmail.com>
2021-09-29 17:02:37 +0100

    task12

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,11 @@
+const roomDimensions = {
+    width: 50,
+    length: 100,
+    getArea() {
+        return this.width * this.length
+    }
+}
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions)
+
+console.log(boundGetArea())
